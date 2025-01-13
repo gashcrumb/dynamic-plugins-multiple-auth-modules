0da19b666b50b6aac564fe9b167765f7668f3b92
Stan Lewis <gashcrumb@gmail.com>
2025-01-13 13:11:33 -0500

    feat(example) create auth provider example
    This change adds an example showing a way to install an authentication
    provider using the dynamic plugins feature of RHDH.  The example
    includes two wrappers that make use of an existing set of plugins and a
    guide that walks through building the example and setting it up.  The
    guide also includes instructions on how to set up a simple LDAP server
    to see this all running in a local environment.
    
    Signed-off-by: Stan Lewis <gashcrumb@gmail.com>

---
+++ plugins/immobiliarelabs-backstage-plugin-ldap-auth-backend/src/index.ts
@@ -0,0 +1,1 @@
+export { default as default } from "@immobiliarelabs/backstage-plugin-ldap-auth-backend";
---
+++ plugins/immobiliarelabs-backstage-plugin-ldap-auth/src/index.ts
@@ -0,0 +1,2 @@
+export * from '@immobiliarelabs/backstage-plugin-ldap-auth';
+export { SignInPage } from './components/SignInPage/SignInPage';
